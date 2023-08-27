2ec35203399c7da14998325eedb19d6a2b953ad1
hassanxdev <143224895+hassanxdev@users.noreply.github.com>
2023-08-27 03:55:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['212614352139'] //ur owner number
+global.ownername = "HASSAN.HEX❤️" //ur owner name
+global.ytname = "YT: REDX.dev" //ur yt chanel name
+global.socialm = "GitHub: H4SS4N.dev" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "212614352139" //ur number
+global.premium = ['212614352139'] //ur premium number
+global.botname = 'HASSAN BOT❤️' //ur bot name
+global.linkz = "https://youtube.com/@H4SS4NXDEV" //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph/file/70a7fc13196dd7eddd98a.png" //ur website to be displayed
+global.botscript = 'https://youtube.com/@H4SS4N.dev' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣HASSAN STICKEY⍣" //ur sticker watermark packname
+global.author = "⍣212614352139⍣" //ur sticker watermark author
+global.wm = "❤️H4SS4NX BOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -40,7 +40,7 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'تم يروحي✨',
+    success: 'DONE✨',
     admin: '『 الامر دا لادمن بس! 』',
     botAdmin: '『 ارفع البوت ادمن الاول 』',
     premime: '『 ميزات خاصة متميزة إذا كنت ترغب في تسجيل نوع الإيجار 』',
